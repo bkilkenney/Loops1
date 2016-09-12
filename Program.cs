7fffa86d86fbe4b94fb898bc69ea8ab76d285348
Brad Kilkenney <bkilkenney@gmail.com>
2016-09-12 14:07:22 -0400

    initial commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loops1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int counter = 0;
+            while(counter < 100)
+            {
+                Console.WriteLine(counter);
+                counter++; 
+            }
+        }
+    }
+}
