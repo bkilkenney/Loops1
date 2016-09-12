e4d7e15bab5a896e31a86b1dc3c85627ec304e75
Brad Kilkenney <bkilkenney@gmail.com>
2016-09-12 15:00:33 -0400

    End of school day commit

--- Program.cs
+++ Program.cs
@@ -10,11 +10,31 @@ namespace Loops1
     {
         static void Main(string[] args)
         {
-            int counter = 0;
-            while(counter < 100)
+            //int counter = 0;
+            //while(counter <= 7)
+            //    {
+            //    Console.WriteLine(counter);
+            //    counter++; //if this line is missed it will continuously do 0
+            //    }
+
+            //int sum = 0;  //setting up variable outside
+            //int anotherCounter = 1; 
+            //while (anotherCounter <= 10)
+            //    {
+            //    sum += anotherCounter;
+            //    anotherCounter++;
+            //    }
+
+            //Console.WriteLine(sum);
+            Console.WriteLine("Pick a number");
+            int counter2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Pick 2nd number higher than the first");
+            int counter3 = int.Parse(Console.ReadLine());
+            while (counter2 <= counter3)
             {
-                Console.WriteLine(counter);
-                counter++; 
+             
+                Console.WriteLine(counter2);
+                counter2++;
             }
         }
     }
